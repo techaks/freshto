48613e56de24e813addfe9b4fa7387b3527f0fb8
techaks <aks22cse@gmail.com>
2025-08-03 08:04:32 +0530

    spiner in bestseller

--- FRONTEND/client/src/Pages/Home.jsx
+++ FRONTEND/client/src/Pages/Home.jsx
@@ -3,15 +3,24 @@ import MainBanner from '../component/MainBanner'
 import Categories from '../component/Categories'
 import Bestseller from '../component/Bestseller'
 import Delivery from '../component/Delivery'
+import Spinner from '../component/Spinner'
+import { UseAppContext } from '../context/AppContext'
 
 
 const Home = () => {
+    const {products} = UseAppContext();
   return (
     <div className='px-15'>
       
         <MainBanner/>
         <Categories/>
-        <Bestseller/>
+
+        {
+          products ? <Bestseller/> : <Spinner/>
+        }
+
+        
+
         <Delivery/>
 
 
